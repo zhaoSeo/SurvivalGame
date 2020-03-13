d363fc17ef537ecfaad0432797f3fff67bfb0b11
zhao Seo <tkddnjs5652@naver.com>
2020-03-13 23:01:26 +0900

    create hand movement

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    public string handName; //맨손 구분
+    public float range; //공격 범위
+    public int damage; //공격력
+    public float workSpeed; //작업속도
+    public float attackDelay; //딜레이
+    public float attackDelayA; //공격 활성화 시점
+    public float attackDelayB; //공격 비활성화 시점
+
+    public Animator anime;
+    //public BoxCollider boxCollider; 인지오차
+}
---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+    //현재 장착된 Hand형 타입 무기
+    [SerializeField]
+    private Hand currentHand;
+
+    //공격중
+    private bool isAttack = false;
+    private bool isSwing = false;
+
+    private RaycastHit hitInfo;
+
+    void Update()
+    {
+        TryAttack();
+    }
+
+    private void TryAttack()
+    {
+        if(Input.GetButton("Fire1"))
+        {
+            if(!isAttack)
+            {
+                StartCoroutine(AttackCoroutine());
+            }
+        }
+    }
+
+    IEnumerator AttackCoroutine()
+    {
+        isAttack = true;
+        currentHand.anime.SetTrigger("Attack");
+        yield return new WaitForSeconds(currentHand.attackDelayA);
+        isSwing = true;
+
+        //공격 활성화 시작
+        StartCoroutine(HitCoroutine());
+
+        yield return new WaitForSeconds(currentHand.attackDelayB);
+        isSwing = false;
+        
+        yield return new WaitForSeconds(currentHand.attackDelay - currentHand.attackDelayA - currentHand.attackDelayB);
+        
+        isAttack = false;
+    }
+    IEnumerator HitCoroutine()
+    {
+        while(isSwing)
+        {
+            if(CheckObject())
+            {
+                isSwing = false;
+                //충돌
+                Debug.Log(hitInfo.transform.name);
+            }
+            yield return null;
+        }
+    }
+    private bool CheckObject()
+    {
+        //transform.TransformDirection(Vector3.forward)
+        if (Physics.Raycast(transform.position, transform.forward, out hitInfo, currentHand.range))
+            return true;
+        return false;
+    }
+}
