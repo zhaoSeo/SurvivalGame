1c2b583d243d25f8bf13c249215f2f245ca1c9c2
zhao Seo <tkddnjs5652@naver.com>
2020-03-14 17:33:40 +0900

    create gun motion

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public string gunName;
+    public float range;
+    public float accuracy;
+    public float fireRate;
+    public float reloadTime;
+
+    public int damage;
+
+    public int reloadBulletCount;
+    public int currentBulletCount;
+    public int maxBulletCount;
+    public int carryBulletCount;
+
+    public float retroActionForce; //반동세기
+    public float retroActionFineSightForce; //정조준시 반동세기
+
+    public Vector3 fineSightOrginPos;
+    public Animator anime;
+
+    public ParticleSystem muzzleFlash;
+    public AudioClip fire_Sound;
+}
---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    [SerializeField]
+    private Gun currentGun;
+
+    private float currentFireRate; //연사속도
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        GunFireRateCal();
+        TryFire();
+    }
+
+    private void GunFireRateCal()
+    {
+        if(currentFireRate > 0)
+        {
+            currentFireRate -= Time.deltaTime; // 1/60
+        }
+    }
+    
+    private void TryFire()
+    {
+        if(Input.GetButton("Fire1") && currentFireRate <= 0)
+        {
+            Fire();
+        }
+    }
+    //발사전
+    private void Fire()
+    {
+        currentFireRate = currentGun.fireRate;
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        Debug.Log("총알 발사함");
+        PlaySE(currentGun.fire_Sound);
+        currentGun.muzzleFlash.Play();
+    }
+
+    private void PlaySE(AudioClip _clip)
+    {
+        audioSource.clip = _clip;
+        audioSource.Play();
+    }
+}
