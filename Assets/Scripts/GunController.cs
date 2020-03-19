976e05c84a8ac17d6d2285e5ea959ab7f6bb91c9
zhao Seo <tkddnjs5652@naver.com>
2020-03-19 20:27:14 +0900

    bullet UI

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -228,4 +228,9 @@ public class GunController : MonoBehaviour
         audioSource.clip = _clip;
         audioSource.Play();
     }
+
+    public Gun GetGun()
+    {
+        return currentGun;
+    }
 }
