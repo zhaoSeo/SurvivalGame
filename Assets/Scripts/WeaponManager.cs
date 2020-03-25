72aeadb11f73596ed216635d79ebf59f450ae982
zhao Seo <tkddnjs5652@naver.com>
2020-03-25 22:21:21 +0900

    Weapon manager

---
+++ Assets/Scripts/WeaponManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[RequireComponent(typeof(GunController))]
+public class WeaponManager : MonoBehaviour
+{
+
+    public static bool isChangeWeapon = false;
+    public static Transform currentWeapon;
+    public static Animator currentWeaponAnime;
+
+    [SerializeField]
+    private string currentWeaponType;
+
+    [SerializeField]
+    private float changeWeaponDealyTime;
+
+    [SerializeField]
+    private float changeWeaponEndDelayTime;
+
+    [SerializeField]
+    private Gun[] guns;
+
+    [SerializeField]
+    private Hand[] hands;
+
+    private Dictionary<string, Gun> gunDictionary = new Dictionary<string, Gun>();
+    private Dictionary<string, Hand> handDictionary = new Dictionary<string, Hand>();
+
+    [SerializeField]
+    private GunController theGunController;
+    [SerializeField]
+    private HandController theHandController;
+
+    void Start()
+    {
+        for(int i = 0; i < guns.Length; i++)
+        {
+            gunDictionary.Add(guns[i].gunName, guns[i]);
+        }
+        for(int i = 0; i < hands.Length; i++)
+        {
+            handDictionary.Add(hands[i].handName, hands[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
